39c36e30c77d52c3d239b79f4da795e4f66fe037
micha <mbladowski@macropage.de>
2021-06-22 23:37:40 +0200

    replace php8 method remove "ns1:" from SOAP request

--- soap_client_finding.php
+++ soap_client_finding.php
@@ -8,7 +8,7 @@ class soap_client_finding extends \SoapClient {
     public function __construct(string $wsdl, array $options = []) {
         $new_options = [];
         foreach ($options as $key => $data) {
-            if (str_contains($key, 'wsdl_')) {
+            if (strpos($key, 'wsdl_') !== false) {
                 $new_options[substr($key,5)] = $data;
             } else {
                 $new_options[$key] = $data;
@@ -25,16 +25,16 @@ class soap_client_finding extends \SoapClient {
      * @param int    $oneWay
      *
      * @return string
+     * @noinspection CallableParameterUseCaseInTypeContextInspection
      */
     public function __doRequest($request, $location, $action, $version, $oneWay = 0): string
     {
-        preg_match_all('/(<\/?(\w*)>)/',$request,$matches);
+        $request = str_replace('xmlns:ns1=', 'xmlns=', $request);
+        preg_match_all('/(<\/ns1:?(\w*)>)/',$request,$matches);
         $toreplace = array_unique($matches[2]);
         foreach ($toreplace as $node) {
-            /** @noinspection CallableParameterUseCaseInTypeContextInspection */
-            $request = preg_replace('/<' . $node . '>/', '<ns1:' . $node . '>', $request);
-            /** @noinspection CallableParameterUseCaseInTypeContextInspection */
-            $request = preg_replace('/<\/' . $node . '>/', '</ns1:' . $node . '>', $request);
+            $request = preg_replace('/<ns1:' . $node . '>/', '<' . $node . '>', $request);
+            $request = preg_replace('/<\/ns1:' . $node . '>/', '</' . $node . '>', $request);
         }
         return parent::__doRequest($request, $location, $action, $version, $oneWay);
     }
